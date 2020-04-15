9567de107d806247575946198ca9f0f6152970b0
Tim Ekl <lithium3141@gmail.com>
2020-04-14 23:08:07 -0700

    Stub in a glossary view controller

---
+++ WorldVent/GlossaryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GlossaryViewController.swift
+//  WorldVent
+//
+//  Created by Timothy Ekl on 4/14/20.
+//  Copyright © 2020 Tim Ekl. All rights reserved.
+//
+
+import UIKit
+
+class GlossaryViewController: UIViewController {
+    
+}
