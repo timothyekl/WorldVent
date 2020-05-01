38d1366b0ab7705bce9c568a3da9cdce7565a02b
Tim Ekl <lithium3141@gmail.com>
2020-04-30 20:28:48 -0700

    Add a "force onboarding" flag
    Place the argument (enabled) in the scheme.
    
    Make sure that we don't allow gesture dismissal on iOS 13+ when the Done
    button is disabled.

--- WorldVent/ViewControllers/OnboardingViewController.swift
+++ WorldVent/ViewControllers/OnboardingViewController.swift
@@ -13,6 +13,9 @@ import UIKit
 /// key is older than the onboarding update version key. The associated value is an integer.
 private let LastCompletedOnboardingVersionKey = "LastCompletedOnboardingVersion"
 
+/// User defaults key that, if set, forces the onboarding screen visible on app launch.
+private let ForceOnboardingVisibleKey = "ForceOnboardingVisible"
+
 /// Int constant for the last time this app updated its onboarding process. Onboarding
 /// will be presented if the user defaults value for the "last version" key is older than
 /// this value.
@@ -25,6 +28,10 @@ class OnboardingTabBarController: UITabBarController {
     }
     
     private static var needsOnboarding: Bool {
+        if UserDefaults.standard.bool(forKey: ForceOnboardingVisibleKey) {
+            return true
+        }
+        
         let lastOnboardingVersion = UserDefaults.standard.integer(forKey: LastCompletedOnboardingVersionKey)
         return lastOnboardingVersion < OnboardingVersion
     }
@@ -44,15 +51,28 @@ class OnboardingTabBarController: UITabBarController {
 class OnboardingViewController: UIViewController {
     @IBOutlet var doneButtonItem: UIBarButtonItem!
     
+    private var allowDismiss: Bool {
+        get {
+            return doneButtonItem.isEnabled
+        }
+        set {
+            doneButtonItem.isEnabled = newValue
+            
+            if #available(iOS 13.0, *) {
+                isModalInPresentation = !newValue
+            }
+        }
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        doneButtonItem.isEnabled = false
+        allowDismiss = false
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.doneButtonItem.isEnabled = true
+            self.allowDismiss = true
         }
     }
 }
